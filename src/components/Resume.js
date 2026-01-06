74b82f0ac88b7c4d3b74a648c0eaa347a0cde6f5
Venkata2405 <karthickvengatam24@gmail.com>
2026-01-06 18:59:11 +0530

    resume-download

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,33 +1,39 @@
-import ResumeImg from '../assets/resume-1.png';
+import ResumeImg from "../assets/resume-1.png";
 
 export default function Resume() {
   return (
-    <section id='Resume' className='flex flex-col md:flex-row px-5 py-5 bg-8 gap-3'>
-      
-      <div className='py-5 md:w-1/2 flex justify-center md:justify-end'>
-        <img className='w-[400px]' src={ResumeImg} alt="Resume" />
+    <section
+      id="Resume"
+      className="flex flex-col md:flex-row px-5 py-10 bg-8 gap-6"
+    >
+      {/* Resume Image */}
+      <div className="md:w-1/2 flex justify-center md:justify-end">
+        <img
+          src={ResumeImg}
+          alt="Resume Preview"
+          className="w-[400px] rounded-lg shadow-lg"
+        />
       </div>
 
-      <div className='md:w-1/2 flex justify-center'>
-        <h1 className='md:px-5 py-20 text-gray-300 text-3xl font-hero-font'>
-          <span className='text-4xl border-b-4 border-gray-300'>
+      {/* Resume Content */}
+      <div className="md:w-1/2 flex items-center justify-center">
+        <div className="text-gray-300 text-center md:text-left font-hero-font">
+          <h1 className="text-4xl mb-6 border-b-4 border-gray-300 inline-block">
             Resume & Certificates
-          </span>
-          <br /><br />
-          <p className='text-gray-300 text-2xl'>
-            You can view my resume and certificates.
-            <br /><br />
-            <a
-              href={`${process.env.PUBLIC_URL}/Resume and Certification- (Venkata Karthick Thiruvenkadam).pdf`}
-              download target="_blank" rel="noreferrer"
-              className="button w-fit font-hero-font"
-            >
-              Download
-            </a>
-          </p>
-        </h1>
-      </div>
+          </h1>
 
+          <p className="text-2xl mb-8">
+            You can view and download my resume and certificates.
+          </p>
+
+          <a
+            href={`${process.env.PUBLIC_URL}/resume.pdf`}
+            className="button px-6 py-3 text-xl inline-block"
+          >
+            Download Resume
+          </a>
+        </div>
+      </div>
     </section>
   );
 }
